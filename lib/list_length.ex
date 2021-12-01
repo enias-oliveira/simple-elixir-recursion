00707aacae70a354cfa6896eea03fdc7a36cf094
Enias Oliveira <eniasoliveira27@gmail.com>
2021-11-30 23:18:15 -0300

    solves list length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  defp helper([_ | tail], acc) do
+    helper(tail, acc + 1)
+  end
+
+  defp helper([], acc) do
+    acc
+  end
+
+  def call(list) do
+    helper(list, 0)
   end
 end
